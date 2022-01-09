71a749d32faafbb2ecd2dd26744aa49bf5b9ff5c
kingjulien <jazdecf3@yahoo.com>
2022-01-09 17:15:00 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// uloha - pridat fetch load start a fetch load end indicator - napriklad pre loading icon
+
+interface IState {
+  temperature: number;
 }
+
+const initialState: IState = {
+  temperature: 24,
+};
+
+const App: React.FC = () => {
+  const [data, setData] = useState<IState>(initialState);
+
+  useEffect(() => {
+    console.log('mounting');
+
+    // returned function will be called on component unmount
+    return () => {
+      alert('just cleaning up..');
+    };
+  }, []); // The empty array causes this effect to only run on mount
+
+  return (
+    <>
+      <div className="App">
+        <img src={logo} alt="React logo" width="100" height="100" />
+      </div>
+      {/* create and use data component here - try lifecycle methods using conditional statement */}
+      <div className={styles.data}>Temperature: {data.temperature}</div>
+    </>
+  );
+};
 
 export default App;
